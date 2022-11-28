717bbd5ce0c3480d74faa5a35fedc6f9f593c0d5
eskiimo <kareeemgm@gmail.com>
2022-11-28 16:28:29 +0200

    80%

--- chess.js
+++ chess.js
@@ -10,48 +10,69 @@ let board = `~~~~~~~~
 ///assuming Capital "white" starts top -> down
 
 const checkMove = (start, end) => {
-  let movementType;
+  let movementType = "";
   let dx = 0;
   let dy = 0;
+  dx = Math.abs(start[0] - end[0]);
+  dy = Math.abs(start[1] - end[1]);
+
+  if (dx <= 1 && dy <= 1) {
+    movementType = "step-";
+  }
+  if ((dx === 1 && dx === 2 * dy) || dx === 0.5 * dy) {
+    movementType = "N";
+  }
   if (start[0] - end[0] !== 0 && start[1] - end[1] !== 0) {
-    movementType = "diagonal";
+    movementType += "diagonal";
   } else if (start[0] - end[0] === 0 && start[1] - end[1] !== 0) {
-    movementType = "vertical";
-    dx = Math.abs(start[0] - end[0]);
-    dy = Math.abs(start[1] - end[1]);
-
-    if (dx <= 1 && dy <= 1) {
-      movementType += " step";
-    }
+    movementType += "vertical";
   } else if (start[0] - end[0] !== 0 && start[1] - end[1] === 0) {
-    movementType = "horizontal";
-
-    dx = Math.abs(start[0] - end[0]);
-    dy = Math.abs(start[1] - end[1]);
-
-    if (dx <= 1 && dy <= 1) {
-      movementType += " step";
-    }
+    movementType += "horizontal";
   }
   return movementType;
 };
 
 const isValidMove = (board, aka, endPos) => {
-  let places = [];
   let x = 1;
   let y = 1;
-  let Ltype;
-  let movementType;
+  const chesslaw = [
+    { kind: "K", moves: ["step-vertical", "step-horizontal", "step-diagonal"] },
+    {
+      kind: "Q",
+      moves: [
+        "diagonal",
+        "horizontal",
+        "vertical",
+        "step-vertical",
+        "step-horizontal",
+        "step-diagonal",
+      ],
+    },
+    { kind: "B", moves: ["diagonal", "step-diagonal"] },
+    { kind: "N", moves: ["N"] },
+    { kind: "p", moves: ["step-horizontal"] },
+    {
+      kind: "R",
+      moves: ["step-horizontal", "step-vertical", "vertical", "horizontal"],
+    },
+  ];
+
   let trimmed = board.replaceAll("\n", "").replaceAll(" ", "");
 
-  if (aka.toUpperCase().charCodeAt(0) - aka.charCodeAt(0) === 0) {
-    Ltype = "Capital";
-  } else {
-    Ltype = "small";
-  }
+  // if (aka.toUpperCase().charCodeAt(0) - aka.charCodeAt(0) === 0) {
+  //   Ltype = "Capital";
+  // } else {
+  //   Ltype = "small";
+  // }
+
+  let aka2 = aka.toUpperCase();
   let step = 0;
   let piece;
   for (let i = 1; i <= trimmed.length; i++) {
+    if (!trimmed.includes(aka)) {
+      console.log(aka, " not on the board ");
+      return;
+    }
     if (trimmed[i] && trimmed[i] !== "~" && trimmed[i] === aka) {
       piece = {
         name: trimmed[i],
@@ -67,14 +88,21 @@ const isValidMove = (board, aka, endPos) => {
       x = 1;
     }
   }
-  console.log(piece.startPos, endPos, piece.movementType);
+  let c = chesslaw.findIndex((e) => e.kind === aka2);
+
+  console.log(
+    chesslaw[c].moves.includes(piece.movementType)
+      ? "true ," + piece.movementType + " is a valid move for " + aka
+      : "false ," + piece.movementType + " is not a valid move " + aka
+  );
 };
 
 const testCases = () => {
-  isValidMove(board, "k", [1, 1]);
+  isValidMove(board, "K", [1, 1]);
   isValidMove(board, "K", [3, 4]);
   isValidMove(board, "b", [5, 2]);
   isValidMove(board, "R", [8, 8]);
+  isValidMove(board, "x", [8, 8]);
 };
 
 testCases();
