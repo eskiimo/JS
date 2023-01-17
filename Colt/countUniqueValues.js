3fa10e12dca99e1c659cbe736cc107d4d3408770
eskiimo <kareeemgm@gmail.com>
2023-01-17 22:51:23 +0200

    problem solving patterns

---
+++ Colt/countUniqueValues.js
@@ -0,0 +1,30 @@
+// my solution using while
+const items = [1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+function cUv(list) {
+  let i = 0;
+  let j = 1;
+  while (j < list.length) {
+    if (list[i] == list[j]) {
+      j++;
+    } else {
+      i++;
+      list[i] = list[j];
+    }
+  }
+  return i + 1;
+}
+console.log(cUv(items));
+
+//  Colt solution
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
